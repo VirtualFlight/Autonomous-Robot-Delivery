b5f4387f1dc70325d2bf891dd4778f80b7d1a951
Jefferson Chen <jefferson8268@hotmail.com>
2025-12-15 22:34:18 -0500

    ultrasonic class fix

--- embedded/ultrasonic.py
+++ embedded/ultrasonic.py
@@ -1,7 +1,10 @@
 from gpiozero import DistanceSensor
 
 class Ultrasonic:
-    def __init__(self):
-        self.sensor = DistanceSensor(echo=24, trigger=23)
+    def __init__(self, echo, trigger):
+        """
+        Parameters: echo pin, trigger pin
+        """
+        self.sensor = DistanceSensor(echo=echo, trigger=trigger)
     def get_distance(self):
         return self.sensor.distance
