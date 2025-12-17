1e8b29f2f27f8dfa5af316045429bc10af3ba9dd
tharuk999 <tharukran@gmail.com>
2025-12-17 01:03:29 -0500

    finished orderHistory + profile frontend

--- frontend/app/Components/NavBar.jsx
+++ frontend/app/Components/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     <div className="fixed left-0 top-0 h-screen w-1/4 bg-zinc-50 font-sans">
       <nav className="flex flex-col h-full w-full gap-8 p-8 bg-white rounded-2xl shadow-lg border border-gray-100">
         <div className="flex gap-2">
-          <Image src="" alt="NavidEats logo" width={80} className="border-2 border-gray-500"/>
+          <Image src="/logo.svg" alt="NavidEats logo" width={80} height={80}/>
           <div>
             <p className="text-5xl font-bold text-gray-800">NavidEats</p>
             <p className="text-gray-500 text-lg mt-1">Customer Portal</p>
@@ -19,7 +19,7 @@ export default function NavBar() {
             href="/" 
             className="flex items-center gap-3 px-4 py-3.5 rounded-lg hover:bg-gray-100 hover:text-gray-700 transition-colors text-gray-700"
           >
-            <span className="text-xl">🍽️</span>
+            <Image src="/home.svg" width={50} height={50} className="text-xl w-8 h-8" />
             <span className="font-medium text-lg">Restaurants</span>
           </Link>
           
@@ -27,16 +27,16 @@ export default function NavBar() {
             href="/ActiveOrders" 
             className="flex items-center gap-3 px-4 py-3.5 rounded-lg hover:bg-gray-100 transition-colors text-gray-700"
           >
-            <span className="text-xl">🚚</span>
+            <Image src="/orders.svg" width={50} height={50} className="text-xl w-8 h-8" />
             <span className="font-medium text-lg">Active Orders</span>
-            <span className="ml-auto bg-red-500 text-white text-xs px-2.5 py-1.5 rounded-full">2</span>
+            <span className="ml-auto bg-red-500 text-white text-xs px-2.5 py-1.5 rounded-full">67</span>
           </Link>
           
           <Link 
             href="/OrderHistory" 
             className="flex items-center gap-3 px-4 py-3.5 rounded-lg hover:bg-gray-100 transition-colors text-gray-700"
           >
-            <span className="text-xl">📋</span>
+            <Image src="/clock.svg" width={50} height={50} className="text-xl w-8 h-8" />
             <span className="font-medium text-lg">Order History</span>
           </Link>
           
@@ -44,7 +44,7 @@ export default function NavBar() {
             href="/Profile" 
             className="flex items-center gap-3 px-4 py-3.5 rounded-lg hover:bg-gray-100 transition-colors text-gray-700"
           >
-            <span className="text-xl">👤</span>
+            <Image src="/profile_icon.svg" width={50} height={50} className="text-xl w-8 h-8" />
             <span className="font-medium text-lg">Profile</span>
           </Link>
         </div>
@@ -53,7 +53,7 @@ export default function NavBar() {
             <Link href="/login" className="flex items-center gap-3 px-4 py-3.5 rounded-lg hover:bg-gray-100 transition-colors text-gray-700">
                 <div className="flex items-center gap-3">
                     <div className="w-12 h-12 bg-red-300 rounded-xl flex items-center justify-center">
-                        <span className="text-red-600 font-semibold text-lg">icon</span>
+                        <Image src="/logout.svg" width={50} height={50} className="text-red-600 font-semibold text-lg p-3" alt="logout icon"/>
                     </div>
                     <div>
                         <p className="font-medium text-red-600">Log Out</p>
--- frontend/app/Components/OrderHistoryItem.jsx
+++ frontend/app/Components/OrderHistoryItem.jsx
@@ -11,7 +11,7 @@ export default function OrderHistoryItem() {
           <div className="flex flex-col border-2 border-gray-200 bg-white w-full h-48 rounded-xl pt-4 pl-4 pr-4 pb-2">
             <div className="flex"> {/* TOP ITEM INFO */}
               <div className="flex w-full gap-4">
-                <Image  width={70} alt="order icon" className="border-2 border-green-500"/>
+                <Image src="/orders_green.svg" width={70} height={70} alt="order icon" className=" bg-[#34C759]/40 rounded-xl p-3 w-16 h-16"/>
                 <div className="flex flex-col w-full justify-between">
                   <p className="text-xl">Green Bowl</p>
                   <p className="text-sm text-gray-500">Order #ORD-1240</p>
@@ -32,15 +32,15 @@ export default function OrderHistoryItem() {
             <div className="flex h-full border-t border-gray-200 mt-4 p-2 justify-between"> {/* RATING & REORDER */}
               <div className="flex items-center">
                 <p className="text-sm">Your Rating: </p>
-                <Image  alt="star" className="border-2 border-red-500 ml-2"/>
-                <Image  alt="star" className="border-2 border-red-500 ml-2"/>
-                <Image  alt="star" className="border-2 border-red-500 ml-2"/>
-                <Image  alt="star" className="border-2 border-red-500 ml-2"/>
-                <Image  alt="star" className="border-2 border-red-500 ml-2"/>
+                <Image src="/star.svg" width={50} height={50} alt="star" className="ml-2 w-5 h-5"/>
+                <Image src="/star.svg" width={50} height={50} alt="star" className="ml-2 w-5 h-5"/>
+                <Image src="/star.svg" width={50} height={50} alt="star" className="ml-2 w-5 h-5"/>
+                <Image src="/star.svg" width={50} height={50} alt="star" className="ml-2 w-5 h-5"/>
+                <Image src="/star.svg" width={50} height={50} alt="star" className="ml-2 w-5 h-5"/>
               </div>
-              <div className="flex gap-2 bg-blue-50 justify-center items-center p-2 w-36 h-full rounded-xl">
-                <Image  alt="star" className="border-2 border-blue-200 ml-2"/>
-                <p className="text-xl text-blue-500">Reorder</p>
+              <div className="flex gap-2 bg-[#2D35C9]/5 justify-center items-center p-2 w-36 h-full rounded-xl">
+                <Image src="redo.svg" height={50} width={50} alt="star" className=" w-6 h-6"/>
+                <p className="text-xl text-[#2D35C9]/90">Reorder</p>
               </div>
             </div>
           </div>
