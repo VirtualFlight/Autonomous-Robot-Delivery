0cd5f7b074f6dce56d36c11b257741c95665bd2f
‎Jahiem ‎ <justanotherinternetrandom@gmail.com>
2025-12-18 05:44:32 -0500

    update backend with tcp information (imu)

--- backend/src/main/java/com/robotdelivery/model/SensorData.java
+++ backend/src/main/java/com/robotdelivery/model/SensorData.java
@@ -15,10 +15,10 @@ public class SensorData {
     @JoinColumn(name = "robot_id", nullable = false)
     private Robot robot;
 
-    @Column(name = "position_x", nullable = false)
+    @Column(name = "position_x")
     private Integer positionX;
 
-    @Column(name = "position_y", nullable = false)
+    @Column(name = "position_y")
     private Integer positionY;
 
     @Column(nullable = false)
@@ -30,7 +30,34 @@ public class SensorData {
     @Column(nullable = false)
     private Boolean rightObstacle;
 
-    @Column(nullable = false)
+    @Column(name = "distance_left")
+    private Double distanceLeft;
+
+    @Column(name = "distance_middle")
+    private Double distanceMiddle;
+
+    @Column(name = "distance_right")
+    private Double distanceRight;
+
+    @Column(name = "accel_x")
+    private Double accelX;
+
+    @Column(name = "accel_y")
+    private Double accelY;
+
+    @Column(name = "accel_z")
+    private Double accelZ;
+
+    @Column(name = "gyro_x")
+    private Double gyroX;
+
+    @Column(name = "gyro_y")
+    private Double gyroY;
+
+    @Column(name = "gyro_z")
+    private Double gyroZ;
+
+    @Column
     private Integer batteryLevel;
 
     @Column(nullable = false, updatable = false)
@@ -50,17 +77,35 @@ public class SensorData {
         this.leftObstacle = builder.leftObstacle;
         this.frontObstacle = builder.frontObstacle;
         this.rightObstacle = builder.rightObstacle;
+        this.distanceLeft = builder.distanceLeft;
+        this.distanceMiddle = builder.distanceMiddle;
+        this.distanceRight = builder.distanceRight;
+        this.accelX = builder.accelX;
+        this.accelY = builder.accelY;
+        this.accelZ = builder.accelZ;
+        this.gyroX = builder.gyroX;
+        this.gyroY = builder.gyroY;
+        this.gyroZ = builder.gyroZ;
         this.batteryLevel = builder.batteryLevel;
     }
 
     public static class SensorBuilder {
         private Robot robot;
-        private Integer positionX = 0;
-        private Integer positionY = 0;
+        private Integer positionX;
+        private Integer positionY;
         private Boolean leftObstacle = false;
         private Boolean frontObstacle = false;
         private Boolean rightObstacle = false;
-        private Integer batteryLevel = 100;
+        private Double distanceLeft;
+        private Double distanceMiddle;
+        private Double distanceRight;
+        private Double accelX;
+        private Double accelY;
+        private Double accelZ;
+        private Double gyroX;
+        private Double gyroY;
+        private Double gyroZ;
+        private Integer batteryLevel;
 
         public SensorBuilder robot(Robot robot) {
             this.robot = robot;
@@ -68,8 +113,8 @@ public class SensorData {
         }
 
         public SensorBuilder position(Integer x, Integer y) {
-            if (x != null) this.positionX = x;
-            if (y != null) this.positionY = y;
+            this.positionX = x;
+            this.positionY = y;
             return this;
         }
 
@@ -80,8 +125,29 @@ public class SensorData {
             return this;
         }
 
+        public SensorBuilder distances(Double left, Double middle, Double right) {
+            this.distanceLeft = left;
+            this.distanceMiddle = middle;
+            this.distanceRight = right;
+            return this;
+        }
+
+        public SensorBuilder accelerometer(Double x, Double y, Double z) {
+            this.accelX = x;
+            this.accelY = y;
+            this.accelZ = z;
+            return this;
+        }
+
+        public SensorBuilder gyroscope(Double x, Double y, Double z) {
+            this.gyroX = x;
+            this.gyroY = y;
+            this.gyroZ = z;
+            return this;
+        }
+
         public SensorBuilder batteryLevel(Integer batteryLevel) {
-            if (batteryLevel != null) this.batteryLevel = batteryLevel;
+            this.batteryLevel = batteryLevel;
             return this;
         }
 
@@ -110,6 +176,33 @@ public class SensorData {
 
     public Boolean getRightObstacle() { return rightObstacle; }
     public void setRightObstacle(Boolean rightObstacle) { this.rightObstacle = rightObstacle; }
+
+    public Double getDistanceLeft() { return distanceLeft; }
+    public void setDistanceLeft(Double distanceLeft) { this.distanceLeft = distanceLeft; }
+
+    public Double getDistanceMiddle() { return distanceMiddle; }
+    public void setDistanceMiddle(Double distanceMiddle) { this.distanceMiddle = distanceMiddle; }
+
+    public Double getDistanceRight() { return distanceRight; }
+    public void setDistanceRight(Double distanceRight) { this.distanceRight = distanceRight; }
+
+    public Double getAccelX() { return accelX; }
+    public void setAccelX(Double accelX) { this.accelX = accelX; }
+
+    public Double getAccelY() { return accelY; }
+    public void setAccelY(Double accelY) { this.accelY = accelY; }
+
+    public Double getAccelZ() { return accelZ; }
+    public void setAccelZ(Double accelZ) { this.accelZ = accelZ; }
+
+    public Double getGyroX() { return gyroX; }
+    public void setGyroX(Double gyroX) { this.gyroX = gyroX; }
+
+    public Double getGyroY() { return gyroY; }
+    public void setGyroY(Double gyroY) { this.gyroY = gyroY; }
+
+    public Double getGyroZ() { return gyroZ; }
+    public void setGyroZ(Double gyroZ) { this.gyroZ = gyroZ; }
 
     public Integer getBatteryLevel() { return batteryLevel; }
     public void setBatteryLevel(Integer batteryLevel) { this.batteryLevel = batteryLevel; }
